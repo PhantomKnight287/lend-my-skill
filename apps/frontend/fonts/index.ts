9fa5f5d3b1422c61a0066c02292de280bfa4310e
phantomknight287 <gillgurpal699@gmail.com>
2022-12-23 22:20:31 +0530

    [frontend] load new fonts

--- apps/frontend/fonts/index.ts
+++ apps/frontend/fonts/index.ts
@@ -1,6 +1,16 @@
-import { Inter } from "@next/font/google";
+import { Inter, Outfit, Space_Grotesk } from "@next/font/google";
 
 export const inter = Inter({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+});
+
+export const spaceGrotest = Space_Grotesk({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+});
+
+export const outfit = Outfit({
   weight: ["400", "500", "600", "700"],
   subsets: ["latin"],
 });
