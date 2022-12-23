ddcd9219880f2068a8fe222a437cd23a7f03757e
phantomknight287 <gillgurpal699@gmail.com>
2022-12-23 22:21:32 +0530

    [frontend] create register route

--- apps/frontend/pages/auth/register.tsx
+++ apps/frontend/pages/auth/register.tsx
@@ -1,62 +1,315 @@
-import { useState } from "react";
-import { Stepper, Button, Group } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import {
+  TextInput,
+  PasswordInput,
+  Text,
+  Paper,
+  Group,
+  Button,
+  Checkbox,
+  Anchor,
+  Stack,
+  useMantineColorScheme,
+  Select,
+} from "@mantine/core";
 import { Container } from "@components/container";
+import clsx from "clsx";
 import { MetaTags } from "@components/meta";
-import { FirstForm } from "@components/fragements";
+import { outfit, spaceGrotest } from "@fonts";
+import Link from "next/link";
+import axios from "axios";
+import { URLBuilder } from "@utils/url";
+import { showNotification } from "@mantine/notifications";
+import { RegisterResponse } from "~/types/response/register";
+import { useSetUser, useUser } from "@hooks/user";
+import { createCookie } from "@helpers/cookie";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { profileImageRouteGenerator } from "@utils/profile";
+import { Countries } from "~/constants";
 
-function Register() {
-  const [active, setActive] = useState(0);
-  const nextStep = () =>
-    setActive((current) => (current < 3 ? current + 1 : current));
-  const prevStep = () =>
-    setActive((current) => (current > 0 ? current - 1 : current));
-  return (
-    <Container>
-      <MetaTags
-        title="Register | Lend my Skill"
-        description="Register to lend your skill to the world"
-      />
-      <Stepper
-        active={active}
-        color="green"
-        onStepClick={setActive}
-        breakpoint="sm"
-      >
-        <Stepper.Step
-          label="First step"
-          description="Create an account"
-          allowStepSelect={active > 0}
-          data-active={active == 0}
-        >
-          <FirstForm />
-        </Stepper.Step>
-        <Stepper.Step
-          label="Second step"
-          description="Verify email"
-          allowStepSelect={active > 1}
-        >
-          Step 2 content: Verify email
-        </Stepper.Step>
-        <Stepper.Step
-          label="Final step"
-          description="Get full access"
-          allowStepSelect={active > 2}
-        >
-          Step 3 content: Get full access
-        </Stepper.Step>
-        <Stepper.Completed>
-          Completed, click back button to get to previous step
-        </Stepper.Completed>
-      </Stepper>
+export default function Register() {
+  const form = useForm({
+    initialValues: {
+      email: "",
+      name: "",
+      password: "",
+      terms: true,
+      username: "",
+      confirmPass: "",
+      country: "",
+    },
 
-      <Group position="center" mt="xl">
-        <Button variant="default" onClick={prevStep}>
-          Back
-        </Button>
-        <Button onClick={nextStep}>Next step</Button>
-      </Group>
-    </Container>
-  );
+    validate: {
+      email: (val) => (/^\S+@\S+$/.test(val) ? null : "Invalid email"),
+      password: (val) =>
+        val.length <= 6
+          ? "Password should include at least 6 characters"
+          : null,
+      username: (val) =>
+        val.length >= 8 ? null : "Username must be at least 8 characters long",
+      confirmPass: (val, values) =>
+        val === values.password ? null : "Passwords do not match",
+    },
+    validateInputOnBlur: true,
+  });
+
+  const { colorScheme } = useMantineColorScheme();
+  const dispatch = useSetUser();
+  const { isReady, replace } = useRouter();
+  const { id } = useUser();
+  const [userType, setUserType] = useState<"client" | "freelancer" | "">("");
+  const [checked, setChecked] = useState<"client" | "freelancer" | "">("");
+
+  function handleSubmit(values: typeof form.values) {
+    const { confirmPass, email, name, password, username, country } = values;
+    axios
+      .post<RegisterResponse>(URLBuilder(`/${userType}/auth/register`), {
+        confirmPassword: confirmPass,
+        email,
+        name,
+        password,
+        username,
+        country,
+      })
+      .then((d) => d.data)
+      .then((d) => {
+        dispatch({
+          type: "SET_USER",
+          payload: {
+            ...d.user,
+            avatarUrl: profileImageRouteGenerator(d.user.username),
+            userType: userType as "client" | "freelancer",
+          },
+        });
+        createCookie("token", d.tokens.auth, 1);
+        createCookie("refreshToken", d.tokens.refresh, 7);
+        showNotification({
+          message: "Successfully Registered",
+          color: "green",
+        });
+      })
+      .catch((err) => {
+        const error = err?.response?.data?.errors?.[0].message;
+        showNotification({
+          message:
+            error || err?.response?.data?.message || "Something went wrong",
+          color: "red",
+        });
+      });
+  }
+
+  useEffect(() => {
+    if (isReady && id) {
+      replace("/");
+    }
+  }, [isReady, id]);
+
+  if (!userType)
+    return (
+      <Container className={clsx("mt-20")} my={40}>
+        <MetaTags
+          title="Register | Lend My Skill"
+          description="Register to Lend Your Skill to world and start earning money"
+        />
+        <Text
+          size="lg"
+          weight={500}
+          className={clsx("text-center text-2xl font-bold mb-4", {
+            [outfit.className]: true,
+            "text-white": colorScheme === "dark",
+          })}
+        >
+          Join as a <br />
+          <span className="text-center bg-gradient-to-r from-[#3b82f6] to-[#2dd4bf] bg-clip-text text-transparent">
+            Client
+          </span>{" "}
+          or{" "}
+          <span className="text-center bg-gradient-to-r from-[#3b82f6] to-[#2dd4bf] bg-clip-text text-transparent">
+            Freelancer
+          </span>
+        </Text>
+
+        <Paper radius="md" p="xl" withBorder>
+          <Group position="center">
+            <Paper
+              radius={"md"}
+              withBorder
+              p="xl"
+              className={clsx(
+                "hover:border-[1px] cursor-pointer text-lg hover:border-pink-400",
+                {
+                  [outfit.className]: true,
+                  "text-white": colorScheme === "dark",
+                  "border-[1px] border-pink-400": checked === "client",
+                }
+              )}
+              onClick={() => {
+                setChecked("client");
+              }}
+            >
+              <img src="/icons/customer-50.png" alt="client" />I am a Client, I
+              want to hire a freelancer
+            </Paper>
+            <Paper
+              radius={"md"}
+              withBorder
+              p="xl"
+              className={clsx(
+                "hover:border-[1px] cursor-pointer text-lg hover:border-pink-400",
+                {
+                  [outfit.className]: true,
+                  "text-white": colorScheme === "dark",
+                  "border-[1px] border-pink-400": checked === "freelancer",
+                }
+              )}
+              onClick={() => {
+                setChecked("freelancer");
+              }}
+            >
+              <img src="/icons/freelancer-50.png" alt="freelancer" />I am a
+              Freelancer, I want to work for clients
+            </Paper>
+            <Button
+              variant="filled"
+              className={clsx("bg-pink-400 hover:bg-pink-500")}
+              onClick={() => {
+                setUserType(checked);
+              }}
+            >
+              Confirm
+            </Button>
+          </Group>
+        </Paper>
+      </Container>
+    );
+
+  if (userType === "client" || userType === "freelancer")
+    return (
+      <>
+        <MetaTags
+          title="Register | Lend My Skill"
+          description="Register to Lend Your Skill to world and start earning money"
+        />
+        <Container className={clsx("mt-20")} my={40}>
+          <Text
+            size="lg"
+            weight={500}
+            className={clsx("text-center text-2xl font-bold mb-4", {
+              [outfit.className]: true,
+              "text-white": colorScheme === "dark",
+            })}
+          >
+            Welcome to <br />
+            <span className="text-center bg-gradient-to-r from-[#3b82f6] to-[#2dd4bf] bg-clip-text text-transparent">
+              Lend My Skill
+            </span>
+          </Text>
+
+          <Paper radius="md" p="xl" withBorder>
+            <Text
+              className={clsx("text-center text-2xl font-bold mb-4", {
+                [outfit.className]: true,
+                "text-white": colorScheme === "dark",
+              })}
+            >
+              {userType === "client"
+                ? "Sign Up to Hire Freelancers"
+                : "Sign Up to Start Earning Money"}
+            </Text>
+            <form onSubmit={form.onSubmit((d) => handleSubmit(d))}>
+              <Stack>
+                <TextInput
+                  label="Name"
+                  placeholder="Your name"
+                  required
+                  {...form.getInputProps("name")}
+                />
+
+                <TextInput
+                  required
+                  label="Email"
+                  placeholder="johndoe@mail.com"
+                  type={"email"}
+                  {...form.getInputProps("email")}
+                />
+
+                <TextInput
+                  required
+                  label="Username"
+                  placeholder="johndoe123"
+                  {...form.getInputProps("username")}
+                />
+
+                <PasswordInput
+                  required
+                  label="Password"
+                  placeholder="Your password"
+                  {...form.getInputProps("password")}
+                />
+                <PasswordInput
+                  required
+                  label="Confirm Password"
+                  placeholder="Your password"
+                  {...form.getInputProps("confirmPass")}
+                />
+                <Select
+                  data={Countries}
+                  required
+                  label="Country"
+                  searchable
+                  placeholder="Select your country"
+                  {...form.getInputProps("country")}
+                  maxDropdownHeight={280}
+                />
+
+                <Checkbox
+                  label={
+                    <>
+                      I agree to the{" "}
+                      <Link
+                        href="/terms"
+                        className={"text-[#3b82f6] hover:underline"}
+                      >
+                        Terms and Conditions
+                      </Link>
+                    </>
+                  }
+                  checked={form.values.terms}
+                  onChange={(event) =>
+                    form.setFieldValue("terms", event.currentTarget.checked)
+                  }
+                />
+              </Stack>
+
+              <Group position="apart" mt="xl">
+                <Anchor
+                  component={Link}
+                  href="/auth/login"
+                  type="button"
+                  size="xs"
+                  className="text-[#3b82f6] hover:underline"
+                >
+                  Already have an account? Login
+                </Anchor>
+                <Button
+                  type="submit"
+                  fullWidth
+                  color="black"
+                  className={clsx("", {
+                    [outfit.className]: true,
+                    "bg-gray-900 hover:bg-black": colorScheme === "light",
+                    "bg-gradient-to-r from-[#3b82f6] to-[#2dd4bf] text-white":
+                      colorScheme === "dark",
+                  })}
+                  disabled={!form.values.terms}
+                >
+                  Register
+                </Button>
+              </Group>
+            </form>
+          </Paper>
+        </Container>
+      </>
+    );
 }
-
-export default Register;
