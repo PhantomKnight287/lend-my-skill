962b2ef8da261b6cce4276a19d7d915b473fa646
phantomknight287 <gillgurpal699@gmail.com>
2022-12-23 22:15:15 +0530

    [backend] create routes for auth of clients

---
+++ apps/backend/src/routes/auth/client/auth.controller.ts
@@ -0,0 +1,141 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
+import { compare, hash } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+import { REFRESH_SECRET, SIGN_SECRET } from 'src/constants';
+import { RefreshToken } from 'src/decorators/refresh/refresh.decorator';
+import { PrismaService } from 'src/services/prisma/prisma.service';
+import { VerificationService } from 'src/services/verification/verification.service';
+import { BuyerRegisterDto, BuyerLoginDto } from 'src/validators/auth.validator';
+
+@Controller('client/auth')
+export class AuthController {
+  constructor(
+    protected prisma: PrismaService,
+    protected verification: VerificationService,
+  ) {}
+
+  @Post('register')
+  async register(@Body() body: BuyerRegisterDto) {
+    const { email, password, username, name, country } = body;
+
+    const oldUserWithSameEmail = await this.prisma.client.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (oldUserWithSameEmail) {
+      throw new HttpException(
+        'User with same email already exists',
+        HttpStatus.CONFLICT,
+      );
+    }
+    const oldUserWithSameUsername = await this.prisma.client.findFirst({
+      where: {
+        username: {
+          equals: username,
+          mode: 'insensitive',
+        },
+      },
+    });
+    if (oldUserWithSameUsername) {
+      throw new HttpException(
+        'User with same username already exists',
+        HttpStatus.CONFLICT,
+      );
+    }
+    const isAlreadyRegistered =
+      await this.verification.isEmailAlreadyRegistered(email, 'seller');
+    if (isAlreadyRegistered) {
+      throw new HttpException(
+        'Email already registered as seller',
+        HttpStatus.CONFLICT,
+      );
+    }
+    const user = await this.prisma.client.create({
+      data: {
+        email,
+        password: await hash(password, 12),
+        username,
+        name,
+        country: country || 'India',
+      },
+      select: {
+        username: true,
+        id: true,
+        name: true,
+      },
+    });
+    const token = sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: '1d',
+    });
+    const refreshToken = sign({ id: user.id }, process.env.REFRESH_TOKEN, {
+      expiresIn: '7d',
+    });
+
+    return {
+      user,
+      tokens: {
+        auth: token,
+        refresh: refreshToken,
+      },
+    };
+  }
+  @Get('refresh')
+  async refresh(@RefreshToken({ serialize: true }) { id }) {
+    const token = sign({ id }, process.env.JWT_SECRET, {
+      expiresIn: '1d',
+    });
+    return {
+      token,
+    };
+  }
+  @Post('login')
+  async login(@Body() body: BuyerLoginDto) {
+    const { email, password } = body;
+    const user = await this.prisma.client.findFirst({
+      where: {
+        email,
+      },
+      select: {
+        username: true,
+        id: true,
+        name: true,
+        avatarUrl: true,
+        password: true,
+      },
+    });
+    if (!user) {
+      throw new HttpException(
+        'No User found with provided email.',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    const isPasswordCorrect = await compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      throw new HttpException('Incorrect Password', HttpStatus.UNAUTHORIZED);
+    }
+    const token = sign({ id: user.id }, SIGN_SECRET, {
+      expiresIn: '1d',
+    });
+    const refreshToken = sign({ id: user.id }, REFRESH_SECRET, {
+      expiresIn: '7d',
+    });
+    delete user.password;
+    return {
+      user,
+      tokens: {
+        auth: token,
+        refresh: refreshToken,
+      },
+    };
+  }
+}
