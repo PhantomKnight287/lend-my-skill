78794d059227e8819a7111dfe3bdad4df07978ac
phantomknight287 <gillgurpal699@gmail.com>
2022-12-25 10:19:15 +0530

    [frontend] fix font in index route

--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -1,14 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
+import useHydrateUserContext from "@hooks/hydrate/user";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <>
-      
-    </>
-  )
+  useHydrateUserContext();
+  return <></>;
 }
