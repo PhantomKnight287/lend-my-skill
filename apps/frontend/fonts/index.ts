ec98bc5a85563f41d34199fdb0bb1cb3dcddc26f
phantomknight287 <gillgurpal699@gmail.com>
2022-12-25 10:18:04 +0530

    [frontend] add `display:"swap"` to fonts

--- apps/frontend/fonts/index.ts
+++ apps/frontend/fonts/index.ts
@@ -3,14 +3,17 @@ import { Inter, Outfit, Space_Grotesk } from "@next/font/google";
 export const inter = Inter({
   weight: ["400", "500", "600", "700"],
   subsets: ["latin"],
+  display: "swap",
 });
 
 export const spaceGrotest = Space_Grotesk({
   weight: ["400", "500", "600", "700"],
   subsets: ["latin"],
+  display: "swap",
 });
 
 export const outfit = Outfit({
   weight: ["400", "500", "600", "700"],
   subsets: ["latin"],
+  display: "swap",
 });
