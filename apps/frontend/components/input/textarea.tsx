86039998c02c989edab8e106f8d45c6ee49e2b36
phantomknight287 <gillgurpal699@gmail.com>
2022-12-25 10:16:45 +0530

    [frontend] create custom `Textarea` component

---
+++ apps/frontend/components/input/textarea.tsx
@@ -0,0 +1,67 @@
+import { outfit } from "@fonts";
+import {
+  Textarea as T,
+  TextareaProps,
+  useMantineColorScheme,
+} from "@mantine/core";
+import clsx from "clsx";
+import { ReactNode } from "react";
+
+interface Props extends TextareaProps {
+  labelString: string;
+  required?: boolean;
+  id: string;
+  minWords?: number;
+  maxWords?: number;
+  wordsComponent?: ReactNode;
+}
+
+export default function Textarea({
+  minWords,
+  maxWords,
+  labelString,
+  wordsComponent,
+  ...props
+}: Props) {
+  const { colorScheme } = useMantineColorScheme();
+  return (
+    <div className="mt-4">
+      <label
+        className={clsx("text-sm font-bold py-2", {
+          [outfit.className]: true,
+          "text-[#495057] ": colorScheme === "light",
+        })}
+        htmlFor={props.id}
+      >
+        {labelString}
+        <span>
+          <span className="text-red-500">*</span>
+        </span>
+      </label>
+      <div
+        className={clsx("border-[1px]  rounded-sm flex flex-col", {
+          "border-[#ced4da]": colorScheme === "light",
+          "border-[#495057]": colorScheme === "dark",
+        })}
+      >
+        <T
+          required
+          placeholder="Enter the description of your job post"
+          labelProps={{
+            className: clsx({
+              [outfit.className]: true,
+            }),
+          }}
+          {...props}
+          classNames={{
+            input: clsx("border-0", {}),
+            ...props.classNames,
+          }}
+          minLength={20}
+          maxLength={1000}
+        />
+        {wordsComponent}
+      </div>
+    </div>
+  );
+}
