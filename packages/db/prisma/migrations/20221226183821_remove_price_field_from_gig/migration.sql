9e92dd85d95bb4881fecac3a23f579be7f1bef72
Gurpal Singh <gillgurpal699@gmail.com>
2022-12-29 12:11:40 +0530

    [db] run prisma migrations

---
+++ packages/db/prisma/migrations/20221226183821_remove_price_field_from_gig/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `price` on the `Gig` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Gig" DROP COLUMN "price";
---
+++ packages/db/prisma/migrations/20221228193758_remove_description_from_order_model/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `description` on the `Order` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Order" DROP COLUMN "description";
