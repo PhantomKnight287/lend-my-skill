a7bdf366b4792ae92786dd4e53c4850ef7a49dcd
Gurpal Singh <gillgurpal699@gmail.com>
2022-12-29 18:06:50 +0530

    [frontend] create new routes

---
+++ apps/frontend/pages/profile/[username]/order/[id].tsx
@@ -0,0 +1,13 @@
+import { Container } from '@components/container'
+import React from 'react'
+
+const OrderPage = () => {
+  
+    return (
+    <Container>
+
+    </Container>
+  )
+}
+
+export default OrderPage
