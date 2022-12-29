cfe1751ea51eb0c782f67e517b8516ede89fc61f
Gurpal Singh <gillgurpal699@gmail.com>
2022-12-29 12:11:04 +0530

    [frontend] create Gigs Service

---
+++ apps/frontend/services/gigs.service.ts
@@ -0,0 +1,28 @@
+import { URLBuilder } from "@utils/url";
+import axios from "axios";
+import { AnswerType } from "db";
+
+interface Props {
+  title: string;
+  description: string;
+  category: string;
+  tags: string[];
+  bannerImage: string;
+  images?: string[];
+  packages: {
+    name: string;
+    description: string;
+    price: number;
+    deliveryDays: number;
+  }[];
+  features: { name: string; includedIn: string[] }[];
+  questions: { question: NamedCurve; required: boolean; type: AnswerType }[];
+}
+
+export async function createGig(props: Props, token: string) {
+  return axios.post(URLBuilder("/gigs/create"), props, {
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  });
+}
