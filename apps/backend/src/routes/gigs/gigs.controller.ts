92d682cc126135617f4ab5960a6ef46385b12dd3
Gurpal Singh <gillgurpal699@gmail.com>
2022-12-29 11:54:54 +0530

    [backend] create gigs route

--- apps/backend/src/routes/gigs/gigs.controller.ts
+++ apps/backend/src/routes/gigs/gigs.controller.ts
@@ -4,13 +4,16 @@ import {
   Get,
   HttpException,
   HttpStatus,
+  Param,
   Post,
   Query,
 } from '@nestjs/common';
+import { nanoid } from 'nanoid';
 import { Token } from 'src/decorators/token/token.decorator';
 import { GigsService } from 'src/services/gigs/gigs.service';
 import { PrismaService } from 'src/services/prisma/prisma.service';
 import { VerificationService } from 'src/services/verification/verification.service';
+import { titleToSlug } from 'src/utils/slug';
 import { CreateGigDto } from 'src/validators/gigs.validator';
 
 @Controller('gigs')
@@ -39,18 +42,24 @@ export class GigsController {
         category: true,
         createdAt: true,
         description: true,
-        price: true,
         bannerImage: true,
         id: true,
         title: true,
         tags: true,
+        slug: true,
         freelancer: {
           select: {
             username: true,
             name: true,
             country: true,
             avatarUrl: true,
+            id: true,
           },
+        },
+      },
+      where: {
+        freelancer: {
+          kycCompleted: true,
         },
       },
       skip: toOutput > 10 ? toOutput - 10 : undefined,
@@ -70,29 +79,182 @@ export class GigsController {
     @Body() body: CreateGigDto,
     @Token({ serialize: true }) { id },
   ) {
-    const { bannerImage, category, description, images, price, tags, title } =
-      body;
-    const { profileCompleted, userFound } = await this.verify.verifySeller(id);
+    const {
+      bannerImage,
+      category,
+      description,
+      images,
+      tags,
+      title,
+      features,
+      packages,
+      questions,
+    } = body;
+    const { userFound } = await this.verify.verifySeller(id);
     if (!userFound)
       throw new HttpException(
         'No user account found with associated authentication token.',
         HttpStatus.NOT_FOUND,
       );
-    if (!profileCompleted)
-      throw new HttpException('Profile not completed.', HttpStatus.BAD_REQUEST);
+
     const isCategoryValid = await this.verify.verifySeller(category);
     if (!isCategoryValid)
       throw new HttpException('Invalid category.', HttpStatus.BAD_REQUEST);
-    const data = await this.gigs.createGig({
-      bannerImage,
-      category,
-      description,
-      images,
-      price,
-      tags,
-      title,
-      userId: id,
+
+    const gig = await this.prisma.gig.create({
+      data: {
+        bannerImage,
+        description,
+        images,
+        tags,
+        title,
+        slug: `${titleToSlug(title)}-${nanoid(10)}`,
+        category: {
+          connect: {
+            id: category,
+          },
+        },
+        freelancer: {
+          connect: {
+            id,
+          },
+        },
+        Package: {
+          create: packages.map((p) => ({
+            deliveryDays: p.deliveryDays,
+            name: p.name,
+            price: p.price,
+            description: p.description,
+            features: {
+              create: features.map((f) => ({
+                name: f.name,
+                includedIn: f.includedIn,
+              })),
+            },
+          })),
+        },
+        questions: {
+          create: questions.map((q) => ({
+            answerType: q.type,
+            question: q.question,
+            isRequired: q.required,
+          })),
+        },
+      },
+      select: {
+        slug: true,
+      },
     });
-    return data;
+    return gig;
+  }
+  @Get(':username/:slug')
+  async getGigBySlug(
+    @Param('username') username: string,
+    @Param('slug') slug: string,
+  ) {
+    const gig = await this.prisma.gig.findFirst({
+      where: {
+        freelancer: {
+          username: {
+            equals: username,
+            mode: 'insensitive',
+          },
+        },
+        slug: {
+          equals: slug,
+          mode: 'insensitive',
+        },
+      },
+      select: {
+        bannerImage: true,
+        category: true,
+        createdAt: true,
+        description: true,
+
+        freelancer: {
+          select: {
+            username: true,
+            profileCompleted: true,
+            name: true,
+            avatarUrl: true,
+            verified: true,
+          },
+        },
+        images: true,
+        id: true,
+        Package: {
+          select: {
+            deliveryDays: true,
+            description: true,
+            features: {
+              select: {
+                id: true,
+                name: true,
+                includedIn: true,
+              },
+            },
+            id: true,
+            name: true,
+            price: true,
+          },
+        },
+        slug: true,
+        title: true,
+        tags: true,
+      },
+    });
+    if (!gig) throw new HttpException('No Gig Found.', HttpStatus.NOT_FOUND);
+    return gig;
+  }
+  @Get(':username')
+  async getGigsByUsername(
+    @Param('username') username: string,
+    @Query('take') take: string,
+  ) {
+    const toTake = Number.isNaN(parseInt(take)) ? 10 : parseInt(take);
+    const gigs = await this.prisma.gig.findMany({
+      where: {
+        freelancer: {
+          username: {
+            equals: username,
+            mode: 'insensitive',
+          },
+        },
+      },
+      select: {
+        freelancer: {
+          select: {
+            username: true,
+            name: true,
+            avatarUrl: true,
+            verified: true,
+            profileCompleted: true,
+          },
+        },
+        createdAt: true,
+        title: true,
+        slug: true,
+        tags: true,
+        id: true,
+        description: true,
+      },
+      orderBy: [
+        {
+          updatedAt: 'desc',
+        },
+        {
+          createdAt: 'desc',
+        },
+      ],
+      take: toTake,
+      skip: toTake > 10 ? toTake - 10 : undefined,
+    });
+    if (gigs.length === toTake) {
+      return {
+        next: toTake + 10,
+        gigs,
+      };
+    }
+    return { gigs };
   }
 }
