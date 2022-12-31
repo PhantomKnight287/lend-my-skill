9de5c638ad9dcc1dd1c73994eeac51853b4f5aff
Gurpal Singh <gillgurpal699@gmail.com>
2022-12-31 13:17:01 +0530

    [mobile] move application's code to `apps/mobile`

---
+++ apps/mobile/lib/components/input.dart
@@ -0,0 +1,53 @@
+import "package:flutter/material.dart";
+import 'package:google_fonts/google_fonts.dart';
+
+class InputField extends StatelessWidget {
+  final String? labelText;
+  final Function(String)? onChanged;
+  final Function(String)? onSubmitted;
+  final String? errorText;
+  final TextInputType? keyboardType;
+  final TextInputAction? textInputAction;
+  final bool autoFocus;
+  final bool obscureText;
+  final Widget? suffix;
+  final TextEditingController? controller;
+  final Widget? prefix;
+  const InputField(
+      {this.labelText,
+      this.onChanged,
+      this.onSubmitted,
+      this.errorText,
+      this.keyboardType,
+      this.textInputAction,
+      this.autoFocus = false,
+      this.obscureText = false,
+      this.suffix,
+      this.controller,
+      this.prefix,
+      Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      autofocus: autoFocus,
+      onChanged: onChanged,
+      onSubmitted: onSubmitted,
+      keyboardType: keyboardType,
+      textInputAction: textInputAction,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+          labelText: labelText,
+          labelStyle: GoogleFonts.spaceGrotesk(),
+          errorText: errorText,
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          suffix: suffix,
+          prefix: prefix),
+      controller: controller,
+    );
+  }
+}
