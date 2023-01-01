bbaabde1755fc5fdd8eb90b069b502ff7952e741
Gurpal Singh <gillgurpal699@gmail.com>
2023-01-01 23:11:04 +0530

    [frontend] remove Accounts section from Navbar

--- apps/frontend/components/sidebars/settings.tsx
+++ apps/frontend/components/sidebars/settings.tsx
@@ -147,26 +147,6 @@ export function SettingsSidebar({
           );
         })}
       </Navbar.Section>
-
-      <Navbar.Section className={classes.footer}>
-        <a
-          href="#"
-          className={classes.link}
-          onClick={(event) => event.preventDefault()}
-        >
-          <IconSwitchHorizontal className={classes.linkIcon} stroke={1.5} />
-          <span>Change account</span>
-        </a>
-
-        <a
-          href="#"
-          className={classes.link}
-          onClick={(event) => event.preventDefault()}
-        >
-          <IconLogout className={classes.linkIcon} stroke={1.5} />
-          <span>Logout</span>
-        </a>
-      </Navbar.Section>
     </Navbar>
   );
 }
