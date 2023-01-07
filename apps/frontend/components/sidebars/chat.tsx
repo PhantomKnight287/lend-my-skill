44c89f8bd2abca18366a44d8638805c6da4c16ee
Gurpal Singh <gillgurpal699@gmail.com>
2023-01-07 18:56:32 +0530

    [frontend] fix chat sidebar

--- apps/frontend/components/sidebars/chat.tsx
+++ apps/frontend/components/sidebars/chat.tsx
@@ -29,95 +29,110 @@ export function ChatSidebar({ client, freelancer }: ChatSidebarProps) {
         "flex-col-reverse": userType === "freelancer",
       })}
     >
-      <div className="flex flex-row items-center hover:scale-110 transition-all duration-[110ms] border-[1px] p-1 rounded-md mb-2 ">
-        <Avatar
-          src={
-            freelancer.avatarUrl
-              ? assetURLBuilder(freelancer.avatarUrl)
-              : profileImageRouteGenerator(freelancer.username)
-          }
-          size="md"
-          radius="xl"
-        />
-        <div className="flex flex-col ml-2">
-          <h2 className="text-base font-semibold overflow-x-hidden">
-            {freelancer.name}
-            {freelancer.verified ? (
-              <Tooltip label="Verified User" withArrow>
-                <Badge
-                  color="green"
-                  variant="light"
-                  className="rounded-full ml-2"
-                >
-                  <div className="flex flex-row flex-nowrap items-center justify-center">
-                    <IconCheck color="green" size={15} />
-                  </div>
-                </Badge>
-              </Tooltip>
-            ) : null}
-          </h2>
-          <Text
-            color={"dimmed"}
-            className={clsx({
-              [outfit.className]: true,
-            })}
-            lineClamp={1}
-          >
-            <a
-              href={`/profile/${freelancer.username}`}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500 hover:underline"
-            >
-              @{freelancer.username}
-            </a>
-          </Text>
-        </div>
-      </div>
-      <div className="flex flex-row items-center hover:scale-110 transition-all duration-[110ms] border-[1px] p-1 rounded-md mb-2">
-        <Avatar
-          src={
-            client.avatarUrl
-              ? assetURLBuilder(client.avatarUrl)
-              : profileImageRouteGenerator(client.username)
-          }
-          size="md"
-          radius="xl"
-        />
-        <div className="flex flex-col ml-2">
+      <>
+        <Text>
           <h2 className="text-base font-semibold">
-            {client.name}
-            {client.verified ? (
-              <Tooltip label="Verified User" withArrow>
-                <Badge
-                  color="green"
-                  variant="light"
-                  className="rounded-full ml-2"
-                >
-                  <div className="flex flex-row flex-nowrap items-center justify-center">
-                    <IconCheck color="green" size={15} />
-                  </div>
-                </Badge>
-              </Tooltip>
-            ) : null}
+            {userType === "client" ? "Freelancer" : "You"}
           </h2>
-          <Text
-            color={"dimmed"}
-            className={clsx({
-              [outfit.className]: true,
-            })}
-          >
-            <a
-              href={`/profile/${client.username}`}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500 hover:underline"
+        </Text>
+
+        <div className="flex flex-row items-center hover:scale-110 transition-all duration-[110ms] border-[1px] p-1 rounded-md mb-2 ">
+          <Avatar
+            src={
+              freelancer.avatarUrl
+                ? assetURLBuilder(freelancer.avatarUrl)
+                : profileImageRouteGenerator(freelancer.username)
+            }
+            size="md"
+            radius="xl"
+          />
+          <div className="flex flex-col ml-2">
+            <h2 className="text-base font-semibold overflow-x-hidden">
+              {freelancer.name}
+              {freelancer.verified ? (
+                <Tooltip label="Verified User" withArrow>
+                  <Badge
+                    color="green"
+                    variant="light"
+                    className="rounded-full ml-2"
+                  >
+                    <div className="flex flex-row flex-nowrap items-center justify-center">
+                      <IconCheck color="green" size={15} />
+                    </div>
+                  </Badge>
+                </Tooltip>
+              ) : null}
+            </h2>
+            <Text
+              color={"dimmed"}
+              className={clsx({
+                [outfit.className]: true,
+              })}
+              lineClamp={1}
             >
-              @{client.username}
-            </a>
-          </Text>
+              <a
+                href={`/profile/${freelancer.username}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 hover:underline"
+              >
+                @{freelancer.username}
+              </a>
+            </Text>
+          </div>
         </div>
-      </div>
+      </>
+      <>
+        <Text>
+          <h2 className="text-base font-semibold">
+            {userType === "client" ? "You" : "Client"}
+          </h2>
+        </Text>
+        <div className="flex flex-row items-center hover:scale-110 transition-all duration-[110ms] border-[1px] p-1 rounded-md mb-2">
+          <Avatar
+            src={
+              client.avatarUrl
+                ? assetURLBuilder(client.avatarUrl)
+                : profileImageRouteGenerator(client.username)
+            }
+            size="md"
+            radius="xl"
+          />
+          <div className="flex flex-col ml-2">
+            <h2 className="text-base font-semibold">
+              {client.name}
+              {client.verified ? (
+                <Tooltip label="Verified User" withArrow>
+                  <Badge
+                    color="green"
+                    variant="light"
+                    className="rounded-full ml-2"
+                  >
+                    <div className="flex flex-row flex-nowrap items-center justify-center">
+                      <IconCheck color="green" size={15} />
+                    </div>
+                  </Badge>
+                </Tooltip>
+              ) : null}
+            </h2>
+            <Text
+              color={"dimmed"}
+              className={clsx({
+                [outfit.className]: true,
+              })}
+            >
+              <a
+                href={`/profile/${client.username}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 hover:underline"
+              >
+                @{client.username}
+              </a>
+            </Text>
+          </div>
+        </div>
+      </>
     </div>
   );
 }
