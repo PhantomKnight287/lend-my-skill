364d32810f3810d22f67064010cde5c4a6268504
Gurpal Singh <gillgurpal699@gmail.com>
2023-01-12 15:51:15 +0530

    [frontend] create `505.tsx`

---
+++ apps/frontend/pages/505.tsx
@@ -0,0 +1,76 @@
+import { useUser } from "@hooks/user";
+import {
+  createStyles,
+  Title,
+  Text,
+  Button,
+  Container,
+  Group,
+} from "@mantine/core";
+import Link from "next/link";
+
+const useStyles = createStyles((theme) => ({
+  root: {
+    paddingTop: 80,
+    paddingBottom: 120,
+    backgroundColor: theme.fn.variant({
+      variant: "filled",
+      color: theme.primaryColor,
+    }).background,
+  },
+
+  label: {
+    textAlign: "center",
+    fontWeight: 900,
+    fontSize: 220,
+    lineHeight: 1,
+    marginBottom: theme.spacing.xl * 1.5,
+    color: theme.colors[theme.primaryColor][3],
+
+    [theme.fn.smallerThan("sm")]: {
+      fontSize: 120,
+    },
+  },
+
+  title: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    textAlign: "center",
+    fontWeight: 900,
+    fontSize: 38,
+    color: theme.white,
+
+    [theme.fn.smallerThan("sm")]: {
+      fontSize: 32,
+    },
+  },
+
+  description: {
+    maxWidth: 540,
+    margin: "auto",
+    marginTop: theme.spacing.xl,
+    marginBottom: theme.spacing.xl * 1.5,
+    color: theme.colors[theme.primaryColor][1],
+  },
+}));
+
+export function ServerError() {
+  const { classes } = useStyles();
+  const { id } = useUser();
+  return (
+    <div className={classes.root}>
+      <Container>
+        <div className={classes.label}>500</div>
+        <Title className={classes.title}>Something bad just happened...</Title>
+        <Text size="lg" align="center" className={classes.description}>
+          Our servers could not handle your request. Don&apos;t worry, our
+          development team was already notified. Try refreshing the page.
+        </Text>
+        <Group position="center">
+          <Link href={id ? "/dashboard" : "/"}>
+            <Button size="md">Take me back to home page</Button>
+          </Link>
+        </Group>
+      </Container>
+    </div>
+  );
+}
