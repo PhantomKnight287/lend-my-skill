8d36c67ebdef1e712320cb468c30246c95eb6338
Gurpal Singh <gillgurpal699@gmail.com>
2023-02-22 20:44:07 +0530

    [backend] run database query in `Token` decorator to check if user is banne

--- apps/backend/src/decorators/token/token.decorator.ts
+++ apps/backend/src/decorators/token/token.decorator.ts
@@ -7,6 +7,7 @@ import {
 import { Request } from 'express';
 import { JwtPayload, verify } from 'jsonwebtoken';
 import { SIGN_SECRET } from 'src/constants';
+import { PrismaService } from 'src/services/prisma/prisma.service';
 
 export interface DecodedJWT extends JwtPayload {
   id: string;
@@ -14,10 +15,10 @@ export interface DecodedJWT extends JwtPayload {
 }
 
 export const Token = createParamDecorator(
-  (
+  async (
     data: { optional?: boolean; serialize?: boolean },
     ctx: ExecutionContext,
-  ): string | DecodedJWT => {
+  ): Promise<string | DecodedJWT> => {
     const request = ctx.switchToHttp().getRequest<Request>();
     const token = request.headers['authorization']?.replace('Bearer ', '');
     if (!token && !data?.optional)
@@ -29,13 +30,30 @@ export const Token = createParamDecorator(
     if (data?.serialize) {
       try {
         jwt = verify(token, SIGN_SECRET) as unknown as DecodedJWT;
-        return jwt;
       } catch (err) {
         throw new HttpException(
           'Invalid or Expired Authentication Token',
           HttpStatus.FORBIDDEN,
         );
       }
+    }
+    const prisma = new PrismaService();
+    const userRole = jwt.userType;
+    const user =
+      userRole === 'client'
+        ? await prisma.client.findUnique({ where: { id: jwt.id } })
+        : await prisma.freelancer.findUnique({ where: { id: jwt.id } });
+    if (!user && !data?.optional) {
+      throw new HttpException(
+        'Authentication Token is required to access this resource.',
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+    if (user.banned && !data?.optional) {
+      throw new HttpException(
+        `You have been banned: ${user.banReason}`,
+        HttpStatus.FORBIDDEN,
+      );
     }
     return data?.serialize ? jwt : token;
   },
