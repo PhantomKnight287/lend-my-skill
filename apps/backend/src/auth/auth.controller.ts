9d0a68bc8fc09ee8767d5885a584722babcdfee2
Gurpal Singh <gillgurpal699@gmail.com>
2023-03-08 22:24:58 +0530

    [frontend] create `/register` route

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -1,13 +1,19 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
+import { CreateUserDto } from './dto/create-auth.dto';
+import { LoginUserDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  async login(@Body() createAuthDto: CreateAuthDto) {
+  async login(@Body() createAuthDto: LoginUserDto) {
     return await this.authService.login(createAuthDto);
+  }
+
+  @Post('register')
+  async register(@Body() body: CreateUserDto) {
+    return await this.authService.create(body);
   }
 }
