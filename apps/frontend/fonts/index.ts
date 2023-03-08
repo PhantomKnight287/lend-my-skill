f35a3c146897e29f29bc52a5e6f416ad534e18b3
Gurpal Singh <gillgurpal699@gmail.com>
2023-03-09 01:31:13 +0530

    [frontend] add export for `Sen` font

--- apps/frontend/fonts/index.ts
+++ apps/frontend/fonts/index.ts
@@ -1,4 +1,4 @@
-import { Inter, Outfit, Space_Grotesk } from "@next/font/google";
+import { Inter, Outfit, Sen, Space_Grotesk } from "@next/font/google";
 
 export const inter = Inter({
   weight: ["400", "500", "600", "700"],
@@ -16,4 +16,10 @@ export const outfit = Outfit({
   weight: ["400", "500", "600", "700"],
   subsets: ["latin"],
   display: "swap",
+});
+
+export const sen = Sen({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400", "700", "800"],
 });
