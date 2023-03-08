48347a1558a61badf832b0bf690bc81be0df9448
Gurpal Singh <gillgurpal699@gmail.com>
2023-03-08 22:24:37 +0530

    [frontend] create `CreateUserDto`

--- apps/backend/src/auth/dto/create-auth.dto.ts
+++ apps/backend/src/auth/dto/create-auth.dto.ts
@@ -1,10 +1,34 @@
-import { IsEmail, IsString, MinLength } from 'class-validator';
+import { Role } from '@prisma/client';
+import {
+  IsEmail,
+  IsEnum,
+  IsOptional,
+  IsString,
+  IsStrongPassword,
+  MinLength,
+} from 'class-validator';
 
-export class CreateAuthDto {
+export class CreateUserDto {
   @IsEmail() email: string;
 
   @IsString()
   @MinLength(8)
+  @IsStrongPassword()
   password: string;
-  
+
+  @IsString()
+  @IsOptional()
+  confirmPassword: string;
+
+  @IsString()
+  country: string;
+
+  @IsString()
+  username: string;
+
+  @IsEnum(Role)
+  role: Role;
+
+  @IsString()
+  name: string;
 }
--- apps/backend/src/auth/dto/update-auth.dto.ts
+++ apps/backend/src/auth/dto/update-auth.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateAuthDto } from './create-auth.dto';
+import { CreateUserDto } from './create-auth.dto';
 
-export class UpdateAuthDto extends PartialType(CreateAuthDto) {}
+export class UpdateAuthDto extends PartialType(CreateUserDto) {}
