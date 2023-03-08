e4ba4f5a21c1449798fa713d9d44e4c0aa738517
Gurpal Singh <gillgurpal699@gmail.com>
2023-03-08 22:47:59 +0530

    [backend] add ratelimit

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,11 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './services/prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
+import { ProfileModule } from './resources/profile/profile.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   controllers: [AppController],
   providers: [AppService, PrismaService],
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    ProfileModule,
+    ThrottlerModule.forRoot({
+      limit: 100,
+      ttl: 60,
+    }),
+  ],
   exports: [PrismaService],
 })
 export class AppModule {}
