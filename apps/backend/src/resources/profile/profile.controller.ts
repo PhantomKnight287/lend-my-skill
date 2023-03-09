ef37f8cfe60a477c137f23725026ba3ef4347354
Gurpal Singh <gillgurpal699@gmail.com>
2023-03-10 00:22:22 +0530

    [backend] create route to get and update profile

--- apps/backend/src/resources/profile/profile.controller.ts
+++ apps/backend/src/resources/profile/profile.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { Token } from 'decorator/token/token.decorator';
+import { UpdateProfileDTO } from './dto/update-profile.dto';
 
 @Controller('profile')
 export class ProfileController {
@@ -9,5 +10,17 @@ export class ProfileController {
   @Get()
   async getProfile(@Token({ serialize: true }) { id }) {
     return await this.profileService.findUser(id);
+  }
+  @Post('update')
+  async updateProfile(
+    @Token({ serialize: true }) { id },
+    @Body() body: UpdateProfileDTO,
+  ) {
+    return await this.profileService.updateUser(id, body);
+  }
+
+  @Get(':username')
+  async getProfileByUsername(@Param('username') username: string) {
+    return await this.profileService.getProfileByUsername(username);
   }
 }
--- apps/backend/src/resources/profile/profile.service.ts
+++ apps/backend/src/resources/profile/profile.service.ts
@@ -1,5 +1,6 @@
 import { HttpException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/services/prisma/prisma.service';
+import { UpdateProfileDTO } from './dto/update-profile.dto';
 
 @Injectable()
 export class ProfileService {
@@ -15,9 +16,55 @@ export class ProfileService {
         name: true,
         username: true,
         profileCompleted: true,
+        role: true,
+        avatarUrl: true,
+        bio: true,
+        country: true,
       },
     });
     if (!user) throw new HttpException('No User Found', 404);
+    return user;
+  }
+  async updateUser(id: string, body: UpdateProfileDTO) {
+    const user = await this.p.user.findFirst({
+      where: {
+        id,
+      },
+    });
+    if (!user) throw new HttpException('No User Found', 404);
+    const { avatarUrl, bio, country } = body;
+    await this.p.user.update({
+      where: {
+        id,
+      },
+      data: {
+        avatarUrl: avatarUrl || user.avatarUrl,
+        bio: bio || user.bio,
+        country: country || user.country,
+      },
+    });
+    return {
+      updated: true,
+    };
+  }
+  async getProfileByUsername(username: string) {
+    const user = await this.p.user.findFirst({
+      where: {
+        username,
+      },
+      select: {
+        id: true,
+        avatarUrl: true,
+        bio: true,
+        country: true,
+        createdAt: true,
+        verified: true,
+        name: true,
+        username: true,
+      },
+    });
+    if (!user) throw new HttpException('No User Found', 404);
+
     return user;
   }
 }
