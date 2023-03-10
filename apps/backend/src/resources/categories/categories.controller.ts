5b2e20076a5bc671d8201e3c40e2580bf46ae557
Gurpal Singh <gillgurpal699@gmail.com>
2023-03-10 21:15:26 +0530

    [backend] get jwt before creating category

--- apps/backend/src/resources/categories/categories.controller.ts
+++ apps/backend/src/resources/categories/categories.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, HttpException, Post } from '@nestjs/common';
+import { Token } from 'decorator/token/token.decorator';
 import { CategoriesService } from './categories.service';
 
 @Controller('categories')
@@ -11,7 +12,7 @@ export class CategoriesController {
   }
 
   @Post('create')
-  async create(@Body('name') name: string) {
+  async create(@Token({ serialize: true }) { id }, @Body('name') name: string) {
     if (!name) throw new HttpException('Name of category is required.', 400);
     return await this.categoriesService.create(name);
   }
