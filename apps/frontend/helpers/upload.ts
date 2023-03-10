fe7a7f1a3e757b7d972a404be53e14c610d59aaf
Gurpal Singh <gillgurpal699@gmail.com>
2023-03-10 20:34:46 +0530

    [frontend] implement folderName query in upload

--- apps/frontend/helpers/upload.ts
+++ apps/frontend/helpers/upload.ts
@@ -1,13 +1,17 @@
 import { URLBuilder } from "@utils/url";
 import axios from "axios";
 
-export async function uploadFiles(files: File[], token: string) {
+export async function uploadFiles(
+  files: File[],
+  token: string,
+  folderName?: string
+) {
   const formData = new FormData();
   files.forEach((file) => {
     formData.append("files", file);
   });
   return await axios.post<{ paths: string[] }>(
-    URLBuilder("/upload/multiple"),
+    URLBuilder(`/upload/multiple?to=${folderName || "images"}`),
     formData,
     {
       headers: {
