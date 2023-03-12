487c6bf319d132ba455b2b3f553e1ae3dc6889ba
Gurpal Singh <gillgurpal699@gmail.com>
2023-03-12 22:16:15 +0530

    [backend] remove User middleware from Categories module

--- apps/backend/src/resources/categories/categories.module.ts
+++ apps/backend/src/resources/categories/categories.module.ts
@@ -1,27 +1,11 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
 import { PrismaService } from 'src/services/prisma/prisma.service';
-import { UserMiddleware } from '../auth/middleware/auth/auth.middleware';
 import { AuthService } from '../auth/auth.service';
 
 @Module({
   controllers: [CategoriesController],
   providers: [CategoriesService, PrismaService, AuthService],
 })
-export class CategoriesModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(UserMiddleware)
-      .exclude({
-        method: RequestMethod.GET,
-        path: 'categories',
-      })
-      .forRoutes(CategoriesController);
-  }
-}
+export class CategoriesModule {}
