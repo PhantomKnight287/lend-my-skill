2d0e42f16726f60905355013b665e415f31727ce
Gurpal Singh <gillgurpal699@gmail.com>
2023-03-12 21:29:02 +0530

    [backend] add middleware in Categories module

--- apps/backend/src/resources/categories/categories.module.ts
+++ apps/backend/src/resources/categories/categories.module.ts
@@ -1,10 +1,27 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
 import { PrismaService } from 'src/services/prisma/prisma.service';
+import { UserMiddleware } from '../auth/middleware/auth/auth.middleware';
+import { AuthService } from '../auth/auth.service';
 
 @Module({
   controllers: [CategoriesController],
-  providers: [CategoriesService, PrismaService],
+  providers: [CategoriesService, PrismaService, AuthService],
 })
-export class CategoriesModule {}
+export class CategoriesModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(UserMiddleware)
+      .exclude({
+        method: RequestMethod.GET,
+        path: 'categories',
+      })
+      .forRoutes(CategoriesController);
+  }
+}
