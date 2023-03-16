f1b77dd0ddd64742ed985eca44e426bda143fb2c
Gurpal Singh <gillgurpal699@gmail.com>
2023-03-16 14:38:15 +0530

    [frontend] update services component

--- apps/frontend/components/tabs/profile/services.tsx
+++ apps/frontend/components/tabs/profile/services.tsx
@@ -1,8 +1,13 @@
 import {
+  Avatar,
   Button,
+  Divider,
+  Group,
+  Image,
   LoadingOverlay,
   Paper,
-  TypographyStylesProvider,
+  SimpleGrid,
+  Text,
   useMantineColorScheme,
 } from "@mantine/core";
 import { useInfiniteQuery } from "@tanstack/react-query";
@@ -13,8 +18,9 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import clsx from "clsx";
 import Link from "next/link";
-import { outfit } from "@fonts";
+import { outfit, sen } from "@fonts";
 import sanitizeHtml, { IOptions } from "sanitize-html";
+import { IconStar } from "@tabler/icons";
 
 dayjs.extend(relativeTime);
 
@@ -36,9 +42,7 @@ interface Props {
 const ServicesTab = ({ username }: Props) => {
   const {
     data,
-    error,
     hasNextPage,
-    refetch,
     fetchNextPage,
     isLoading,
     isFetchingNextPage,
@@ -46,16 +50,24 @@ const ServicesTab = ({ username }: Props) => {
   } = useInfiniteQuery<{
     services: {
       id: string;
-      freelancer: {
+      user: {
         username: string;
         name: string;
         avatarUrl: string;
       };
       createdAt: string;
       title: string;
-      tags: string[];
+      tags: { name: string; id: string; slug: string }[];
       slug: true;
       description: string;
+      package: [
+        {
+          price: number;
+        }
+      ];
+      rating: number;
+      bannerImage: string;
+      ratedBy: number;
     }[];
     next?: number;
   }>({
@@ -73,56 +85,107 @@ const ServicesTab = ({ username }: Props) => {
     <div className={clsx("container")}>
       <LoadingOverlay visible={isLoading} overlayBlur={2} />
       {data?.pages.map((page, index) => (
-        <Fragment key={index}>
+        <SimpleGrid
+          key={index}
+          cols={3}
+          verticalSpacing="lg"
+          breakpoints={[
+            { maxWidth: 980, cols: 3, spacing: "md" },
+            { maxWidth: 755, cols: 2, spacing: "sm" },
+            { maxWidth: 600, cols: 1, spacing: "sm" },
+          ]}
+        >
           {page.services?.map((service) => (
-            <div key={service.id} className="flex flex-col">
-              <Paper
-                withBorder
-                radius="md"
-                p="md"
-                my="sm"
-                className={clsx("cursor-pointer")}
-                component={Link}
-                href={`/profile/${username}/service/${service.slug}`}
-              >
-                <div className="flex flex-row ">
-                  <div className="flex flex-row items-center">
-                    <img
-                      src={
-                        service.freelancer.avatarUrl
-                          ? assetURLBuilder(service.freelancer.avatarUrl)
-                          : profileImageRouteGenerator(service.freelancer.username)
-                      }
-                      className="w-10 h-10 rounded-full"
-                      alt="avatar"
-                    />
-                    <div className="flex flex-col ml-2">
-                      <span className="font-bold">{service.title}</span>
-                      <span className="text-gray-500 text-sm">
-                        @{service.freelancer.username}
-                      </span>
-                    </div>
-                  </div>
-                  <div className="flex flex-row items-center ml-auto">
-                    <span className={clsx("text-gray-500 text-sm ml-4")}>
-                      {dayjs(service.createdAt).fromNow()}
+            <Paper key={service.id} withBorder shadow={"md"} radius="md">
+              <Image
+                src={assetURLBuilder(service.bannerImage)}
+                width="100%"
+                height={"100%"}
+                alt="Service Banner"
+                classNames={{
+                  image: "rounded-t-md",
+                }}
+              />
+              <Group position="left" mt="md" pl="md">
+                <div>
+                  <Avatar
+                    size="md"
+                    src={
+                      service.user.avatarUrl
+                        ? assetURLBuilder(service.user.avatarUrl)
+                        : profileImageRouteGenerator(service.user.username)
+                    }
+                    radius="xl"
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <Text size="md" className={clsx(sen.className, "mb-0")}>
+                    {service.user.name}
+                  </Text>
+
+                  <Text
+                    size="xs"
+                    className={clsx(sen.className, "mt-0 leading-3")}
+                  >
+                    @{service.user.username}{" "}
+                  </Text>
+                </div>
+              </Group>
+              <Group mt="sm" mb="xs" p="md">
+                <Link
+                  href={`/profile/${username}/service/${service.slug}`}
+                  className="hover:text-white"
+                >
+                  {service.title}
+                </Link>
+              </Group>
+              <Divider />
+              <Group position="apart">
+                <div className="flex flex-col">
+                  <Text size="xs" className={clsx(sen.className)}>
+                    {service.tags.map((tag) => (
+                      <Fragment key={tag.id}>
+                        <Link href={`/t/${tag.slug}`}>
+                          <span className="text-gray-500"># {tag.name} </span>
+                        </Link>
+                      </Fragment>
+                    ))}
+                  </Text>
+                </div>
+              </Group>
+              <Divider />
+              <Group position="apart" p="md">
+                <div className="flex flex-row pl-4">
+                  <IconStar color="yellow" fill="yellow" width={15} />
+                  <span className="ml-1">
+                    {service.rating}
+                    <span className="text-sm text-gray-500 ml-1">
+                      ({service.ratedBy})
                     </span>
-                  </div>
+                  </span>
                 </div>
-                <div className="flex flex-row mt-2">
-                  <TypographyStylesProvider style={{ lineClamp: 2 }}>
-                    <div
-                      dangerouslySetInnerHTML={sanitize(
-                        service.description,
-                        undefined
-                      )}
-                    />
-                  </TypographyStylesProvider>
+                <div className="flex flex-col pr-4">
+                  <Text
+                    className={clsx(
+                      outfit.className,
+                      "text-[10px] mb-0 uppercase"
+                    )}
+                  >
+                    Starting At
+                  </Text>
+                  <Text
+                    className={clsx(
+                      sen.className,
+                      "text-lg mt-0 leading-3 mb-2"
+                    )}
+                  >
+                    $ {service.package[0].price}
+                  </Text>
                 </div>
-              </Paper>
-            </div>
+              </Group>
+            </Paper>
           ))}
-        </Fragment>
+        </SimpleGrid>
       ))}
       <div>
         {isFetchingNextPage ? (
@@ -148,7 +211,8 @@ const ServicesTab = ({ username }: Props) => {
       {data?.pages[0].services.length === 0 && (
         <div className="flex flex-col items-center justify-center w-[100%] container">
           <p>
-            <span className="font-bold">{username}</span> has not posted any service
+            <span className="font-bold">{username}</span> has not posted any
+            service
             <p className="opacity-0">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
               perspiciatis velit. Magni, error reprehenderit quidem provident
