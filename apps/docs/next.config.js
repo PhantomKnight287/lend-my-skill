94c0e6c922aedd15e48618c8f6023129b2e7897f
Gurpal Singh <gillgurpal699@gmail.com>
2023-03-16 15:02:13 +0530

    [docs] update `assetPrefix` and `basePath` in `next.config.js`

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -2,6 +2,8 @@ const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
 });
+
+const isProd = process.env.NODE_ENV === "production";
 /**
  * @type {import('next').NextConfig}
  */
@@ -11,4 +13,6 @@ module.exports = {
     unoptimized: true,
   },
   reactStrictMode: true,
+  assetPrefix: isProd ? "https://phantomknight287.github.io/lend-my-skill" : "",
+  basePath: isProd ? "/lend-my-skill" : "",
 };
