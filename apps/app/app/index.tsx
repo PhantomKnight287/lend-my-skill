8dd3a4cdd61204e8f0f1332908720b343e2b6020
Gurpal Singh <gillgurpal699@gmail.com>
2023-03-16 09:33:02 +0530

    [app] add nativewind for tailwindcss

--- apps/app/app/index.tsx
+++ apps/app/app/index.tsx
@@ -1,34 +1,14 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Page() {
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
+    <View className="flex-1 items-center p-6">
+      <View className="flex-1 justify-center max-w-[960px] mx-auto">
+        <Text className="text-6xl font-bold">Hello World</Text>
+        <Text className="text-4xl text-[#38434d]">
+          This is the first page of your app.
+        </Text>
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});
--- apps/app/babel.config.js
+++ apps/app/babel.config.js
@@ -2,9 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [
-      // NOTE: `expo-router/babel` is a temporary extension to `babel-preset-expo`.
-      require.resolve("expo-router/babel"),
-    ],
+    plugins: ["nativewind/babel", require.resolve("expo-router/babel")],
   };
 };
