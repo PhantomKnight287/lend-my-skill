932104db601ea836653c8c1b08fb2bbd95cc6271
Gurpal Singh <gillgurpal699@gmail.com>
2023-03-19 00:27:07 +0530

    [backend] implement UserMiddleware for each module

--- apps/backend/src/resources/categories/categories.module.ts
+++ apps/backend/src/resources/categories/categories.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
 import { PrismaService } from 'src/services/prisma/prisma.service';
 import { AuthService } from '../auth/auth.service';
+import { UserMiddleware } from '../auth/middleware/auth/auth.middleware';
 
 @Module({
   controllers: [CategoriesController],
   providers: [CategoriesService, PrismaService, AuthService],
 })
-export class CategoriesModule {}
+export class CategoriesModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(UserMiddleware).forRoutes('categories/create');
+  }
+}
--- apps/backend/src/resources/upload/upload.module.ts
+++ apps/backend/src/resources/upload/upload.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { UploadController } from './upload.controller';
 import { PrismaService } from 'src/services/prisma/prisma.service';
+import { UserMiddleware } from '../auth/middleware/auth/auth.middleware';
+import { AuthService } from '../auth/auth.service';
 
 @Module({
   controllers: [UploadController],
-  providers: [UploadService, PrismaService],
+  providers: [UploadService, PrismaService, AuthService],
 })
-export class UploadModule {}
+export class UploadModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(UserMiddleware).forRoutes('upload/(.*)');
+  }
+}
