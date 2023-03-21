6ce98157b38a74c8a5a3ecda8953485ded997dcb
Gurpal Singh <gillgurpal699@gmail.com>
2023-03-21 14:25:13 +0530

    [app] setup `expo-router` and `tailwind-rn`

---
+++ apps/mobile/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./app/**/*.{tsx,js,jsx,ts}", "./components/**/*.{tsx,js,jsx,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: require("tailwind-rn/unsupported-core-plugins"),
+};
