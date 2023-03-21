1f0c24c6ce05eb3b68601a21954679540db3f9a0
Gurpal Singh <gillgurpal699@gmail.com>
2023-03-21 12:48:51 +0530

    [backend] create new routes in job post controller

--- apps/backend/src/resources/job-post/job-post.controller.ts
+++ apps/backend/src/resources/job-post/job-post.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { BodyWithUser } from 'src/types/body';
 import { CreateJobPostDTO } from './dto/create.dto';
 import { JobPostService } from './job-post.service';
@@ -11,5 +11,20 @@ export class JobPostController {
   async createJobPost(@Body() body: BodyWithUser<CreateJobPostDTO>) {
     const { user, ...data } = body;
     return await this.jobPostService.createJobPost(data, user.id);
+  }
+
+  @Get(':username/:slug')
+  async getJobPost(
+    @Param('username') username: string,
+    @Param('slug') slug: string,
+  ) {
+    return await this.jobPostService.getJobPost(username, slug);
+  }
+  @Get(':username')
+  async getJobPostsByUsername(
+    @Param('username') username: string,
+    @Query('take') take?: string,
+  ) {
+    return await this.jobPostService.getJobPostsByUsername(username, take);
   }
 }
