0da8529e56b98c2712c380ede8abed4d6cfcf2ac
Gurpal Singh <gillgurpal699@gmail.com>
2023-03-21 12:49:13 +0530

    [backend] add new methods in hob post service

--- apps/backend/src/resources/job-post/job-post.service.ts
+++ apps/backend/src/resources/job-post/job-post.service.ts
@@ -43,4 +43,113 @@ export class JobPostService {
     });
     return post;
   }
+  async getJobPost(username: string, slug: string) {
+    const post = await this.p.jobPost.findFirst({
+      where: {
+        slug,
+        author: {
+          username: {
+            equals: username,
+            mode: 'insensitive',
+          },
+        },
+      },
+      select: {
+        author: {
+          select: {
+            name: true,
+            verified: true,
+            avatarUrl: true,
+            username: true,
+            profileCompleted: true,
+          },
+        },
+        budget: true,
+        category: {
+          select: {
+            name: true,
+            slug: true,
+            id: true,
+          },
+        },
+        deadline: true,
+        description: true,
+        images: true,
+        slug: true,
+        title: true,
+        tags: {
+          select: {
+            name: true,
+            id: true,
+            slug: true,
+          },
+        },
+        createdAt: true,
+        id: true,
+        claimed: true,
+        claimedBy: {
+          select: {
+            username: true,
+            name: true,
+            avatarUrl: true,
+          },
+        },
+      },
+    });
+    if (!post)
+      throw new HttpException('No post found with provided slug.', 404);
+    return post;
+  }
+  async getJobPostsByUsername(username: string, take?: string) {
+    const toTake = Number.isNaN(Number(take)) ? 10 : Number(take);
+    const posts = await this.p.jobPost.findMany({
+      where: {
+        author: {
+          username: {
+            equals: username,
+            mode: 'insensitive',
+          },
+        },
+      },
+      select: {
+        author: {
+          select: {
+            name: true,
+            verified: true,
+            avatarUrl: true,
+            username: true,
+          },
+        },
+        budget: true,
+        category: {
+          select: {
+            name: true,
+            slug: true,
+          },
+        },
+        deadline: true,
+        description: true,
+        images: true,
+        slug: true,
+        title: true,
+        tags: {
+          select: {
+            name: true,
+            id: true,
+            slug: true,
+          },
+        },
+        createdAt: true,
+        id: true,
+      },
+    });
+    if (!posts) throw new HttpException('No posts found for this user.', 404);
+    if (posts.length === 10) {
+      return {
+        posts,
+        next: toTake + 10,
+      };
+    }
+    return { posts };
+  }
 }
