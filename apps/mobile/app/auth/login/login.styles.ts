ac079598fb92b488c3cc5a4fe7479d2ce8861b45
Gurpal Singh <gillgurpal699@gmail.com>
2023-03-25 02:18:47 +0530

    [app] move file from `/app/login` to `/app/auth/login`

---
+++ apps/mobile/app/auth/login/login.styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+export const LoginStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    backgroundColor: "rgb(213,209,219)",
+    color: "white",
+    paddingHorizontal: 4,
+    paddingVertical: 2,
+  },
+});
--- apps/mobile/app/login/login.styles.ts
+++
@@ -1,2 +0,0 @@
-import { StyleSheet } from "react-native";
-export const STYLES = StyleSheet.create({});
