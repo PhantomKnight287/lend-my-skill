3e7c2a77bec4a3b22b4e07fb88807d866ba8a921
Gurpal Singh <gillgurpal699@gmail.com>
2023-03-25 02:17:23 +0530

    [app] load fonts in `_layout`

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,21 +1,60 @@
-import React from "react";
+import React, { ReactNode, useCallback } from "react";
 import { Stack } from "expo-router";
-import { TailwindProvider } from "tailwind-rn";
-import utilities from "../tailwind.json";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { hideAsync, preventAutoHideAsync } from "expo-splash-screen";
+import { ScrollView, View } from "react-native";
+import {
+  Outfit_100Thin,
+  Outfit_200ExtraLight,
+  Outfit_300Light,
+  Outfit_400Regular,
+  Outfit_500Medium,
+  Outfit_600SemiBold,
+  Outfit_700Bold,
+  Outfit_800ExtraBold,
+  Outfit_900Black,
+  useFonts,
+} from "@expo-google-fonts/outfit";
 
-export const unstable_settings = {
-  initalRouteName: "test",
-};
+preventAutoHideAsync();
 
 export default function Layout() {
+  const [fontLoaded] = useFonts({
+    Outfit_100Thin,
+    Outfit_200ExtraLight,
+    Outfit_300Light,
+    Outfit_400Regular,
+    Outfit_500Medium,
+    Outfit_600SemiBold,
+    Outfit_700Bold,
+    Outfit_800ExtraBold,
+    Outfit_900Black,
+  });
+  const onLayoutRootView = useCallback(async () => {
+    if (fontLoaded) {
+      await hideAsync();
+    }
+  }, [fontLoaded]);
+  if (!fontLoaded) {
+    return null;
+  }
   return (
-    //@ts-ignore: trust me bro
-    <TailwindProvider utilities={utilities}>
-      <Stack
-        screenOptions={{
-          headerTitle: () => null,
-        }}
-      />
-    </TailwindProvider>
+    <SafeAreaProvider>
+      <View onLayout={onLayoutRootView} style={{ flex: 1 }}>
+        <ScrollView contentContainerStyle={{ flex: 1 }}>
+          <SafeAreaHandler>
+            <Stack
+              screenOptions={{
+                headerTitle: () => null,
+              }}
+            />
+          </SafeAreaHandler>
+        </ScrollView>
+      </View>
+    </SafeAreaProvider>
   );
 }
+
+const SafeAreaHandler = (props: { children: ReactNode }) => {
+  return <SafeAreaView style={{ flex: 1 }}>{props.children}</SafeAreaView>;
+};
