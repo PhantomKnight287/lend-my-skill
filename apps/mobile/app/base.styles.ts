7406e75951bcff42120a360aa088c1ed342187d1
Gurpal Singh <gillgurpal699@gmail.com>
2023-03-25 02:17:33 +0530

    [app] create `base.styles.ts`

---
+++ apps/mobile/app/base.styles.ts
@@ -0,0 +1,39 @@
+import { StyleSheet } from "react-native";
+
+export const BaseStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  centeredContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  font100: {
+    fontFamily: "Outfit_100Thin",
+  },
+  font200: {
+    fontFamily: "Outfit_200ExtraLight",
+  },
+  font300: {
+    fontFamily: "Outfit_300Light",
+  },
+  font400: {
+    fontFamily: "Outfit_400Regular",
+  },
+  font500: {
+    fontFamily: "Outfit_500Medium",
+  },
+  font600: {
+    fontFamily: "Outfit_600SemiBold",
+  },
+  font700: {
+    fontFamily: "Outfit_700Bold",
+  },
+  font800: {
+    fontFamily: "Outfit_800ExtraBold",
+  },
+  font900: {
+    fontFamily: "Outfit_900Black",
+  },
+});
