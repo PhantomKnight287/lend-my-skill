9208b0c3dd471842a5572aa7608f9f13921023e3
Gurpal Singh <gillgurpal699@gmail.com>
2023-03-29 21:28:28 +0530

    [backend] create search route

--- apps/backend/src/resources/search/search.controller.ts
+++ apps/backend/src/resources/search/search.controller.ts
@@ -1,7 +1,33 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Query,
+} from '@nestjs/common';
 import { SearchService } from './search.service';
 
 @Controller('search')
 export class SearchController {
   constructor(private readonly searchService: SearchService) {}
+
+  @Get()
+  async search(
+    @Query('q') query: string,
+    @Query('type') sType: 'Job' | 'Service',
+    @Query('take') take?: string,
+  ) {
+    if (!query)
+      throw new HttpException(
+        `Search parameter is required`,
+        HttpStatus.BAD_REQUEST,
+      );
+    if (sType !== 'Service' && sType !== 'Job') {
+      throw new HttpException(
+        'Please Specify type of response required. Possible values are "Job" and "Service"',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    return await this.searchService.search(query, sType, take);
+  }
 }
