890348933666c3be84d6703e33be67eb777b3117
Gurpal Singh <gillgurpal699@gmail.com>
2023-03-29 21:28:16 +0530

    [backend] create `search` method in `SearchService`

--- apps/backend/src/resources/search/search.service.ts
+++ apps/backend/src/resources/search/search.service.ts
@@ -1,4 +1,163 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/services/prisma/prisma.service';
+
+enum SearchType {
+  Job,
+  Service,
+}
 
 @Injectable()
-export class SearchService {}
+export class SearchService {
+  constructor(protected p: PrismaService) {}
+
+  async search(
+    query: string,
+    sType: keyof typeof SearchType = 'Service',
+    take?: string,
+  ) {
+    const toTake = Number.isNaN(parseInt(take)) ? 10 : parseInt(take);
+    if (sType === 'Job') {
+      const jobs = await this.p.jobPost.findMany({
+        where: {
+          OR: [
+            {
+              title: {
+                contains: query,
+                mode: 'insensitive',
+              },
+            },
+            {
+              description: {
+                contains: query,
+                mode: 'insensitive',
+              },
+            },
+          ],
+        },
+        select: {
+          category: {
+            select: {
+              name: true,
+              slug: true,
+            },
+          },
+          id: true,
+          slug: true,
+          user: {
+            select: {
+              id: true,
+              name: true,
+              username: true,
+              country: true,
+              avatarUrl: true,
+              verified: true,
+            },
+          },
+          title: true,
+          createdAt: true,
+          description: true,
+
+          tags: {
+            select: {
+              name: true,
+              id: true,
+              slug: true,
+            },
+          },
+        },
+        take: toTake,
+        skip: toTake > 10 ? toTake - 10 : undefined,
+        orderBy: [
+          {
+            createdAt: 'desc',
+          },
+        ],
+      });
+      if (jobs.length === 10) {
+        return {
+          jobs,
+          next: toTake + 10,
+        };
+      }
+      return { jobs };
+    }
+    const services = await this.p.service.findMany({
+      where: {
+        OR: [
+          {
+            title: {
+              contains: query,
+              mode: 'insensitive',
+            },
+          },
+          {
+            description: {
+              contains: query,
+              mode: 'insensitive',
+            },
+          },
+        ],
+      },
+      select: {
+        category: {
+          select: {
+            name: true,
+            slug: true,
+          },
+        },
+        id: true,
+        slug: true,
+        user: {
+          select: {
+            id: true,
+            name: true,
+            username: true,
+            country: true,
+            avatarUrl: true,
+            verified: true,
+          },
+        },
+        title: true,
+        createdAt: true,
+        description: true,
+        bannerImage: true,
+        package: {
+          select: {
+            price: true,
+          },
+          take: 1,
+          orderBy: [
+            {
+              price: 'asc',
+            },
+          ],
+        },
+        rating: true,
+        tags: {
+          select: {
+            name: true,
+            id: true,
+            slug: true,
+          },
+        },
+        ratedBy: true,
+      },
+      take: toTake,
+      skip: toTake > 10 ? toTake - 10 : undefined,
+      orderBy: [
+        {
+          createdAt: 'desc',
+        },
+      ],
+    });
+    if (services.length === 10) {
+      return {
+        services,
+        next: toTake + 10,
+      };
+    }
+    return {
+      services,
+    };
+  }
+}
