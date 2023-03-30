2e6b7bb1bdffc6a235f5f1bf8a835d69be050568
Gurpal Singh <gillgurpal699@gmail.com>
2023-03-30 23:55:44 +0530

    [backend] create route to search using a category

--- apps/backend/src/resources/search/search.controller.ts
+++ apps/backend/src/resources/search/search.controller.ts
@@ -3,6 +3,7 @@ import {
   Get,
   HttpException,
   HttpStatus,
+  Param,
   Query,
 } from '@nestjs/common';
 import { SearchService } from './search.service';
@@ -29,5 +30,31 @@ export class SearchController {
       );
     }
     return await this.searchService.search(query, sType, take);
+  }
+
+  @Get('category/:category')
+  async searchByCategory(
+    @Query('q') query: string,
+    @Param('category') category: string,
+    @Query('type') sType: 'Job' | 'Service',
+    @Query('take') take?: string,
+  ) {
+    if (!query)
+      throw new HttpException(
+        `Search parameter is required`,
+        HttpStatus.BAD_REQUEST,
+      );
+    if (sType !== 'Service' && sType !== 'Job') {
+      throw new HttpException(
+        `Please Specify type of response required. Possible values are "Job" and "Service"`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    return await this.searchService.searchByCategory(
+      category,
+      query,
+      sType,
+      take,
+    );
   }
 }
