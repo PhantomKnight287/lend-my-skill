51e9fe651a580ace6ac9735e1e103f9f0330d440
Gurpal Singh <gillgurpal699@gmail.com>
2023-05-04 23:53:08 +0530

    [backend] create auth resource

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './services/prisma/prisma.service';
+import { AuthModule } from './resources/auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
