3ce0506dff8ad4216dc5364a4b68963c015aa179
Gurpal Singh <gillgurpal699@gmail.com>
2023-05-14 23:22:06 +0530

    [frontend] update tailwind config

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#e5e5e5",
+        black: "#000814",
+        primary: "#ffd60a",
+        secondary: "#ebf2ff",
+        accent: "#fbff05",
+      },
+    },
   },
   plugins: [],
-}
+};
