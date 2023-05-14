8e1120e893bc35574bd76933a66f322d79586df8
Gurpal Singh <gillgurpal699@gmail.com>
2023-05-14 11:12:21 +0530

    [frontend] update Service page to use tabs

--- apps/frontend/pages/service/[slug]/index.tsx
+++ apps/frontend/pages/service/[slug]/index.tsx
@@ -1,4 +1,3 @@
-import { Container } from "@components/container";
 import { outfit } from "@fonts";
 import { useUser } from "@hooks/user";
 import {
@@ -7,10 +6,11 @@ import {
   Button,
   Divider,
   Image,
+  Rating,
   Table,
+  Tabs,
   Text,
   Tooltip,
-  TypographyStylesProvider,
   useMantineColorScheme,
 } from "@mantine/core";
 import { profileImageRouteGenerator } from "@utils/profile";
@@ -29,6 +29,7 @@ import useHydrateUserContext from "@hooks/hydrate/user";
 import { MetaTags } from "@components/meta";
 import {
   IconCheck,
+  IconClockCheck,
   IconPoint,
   IconPointFilled,
   IconX,
@@ -36,6 +37,7 @@ import {
 import { sanitize } from "@components/tabs/profile/services";
 import { upperFirst } from "@mantine/hooks";
 import useIssueNewAuthToken from "@hooks/jwt";
+import Editor from "@components/editor";
 
 const Service: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = (
   props
@@ -48,7 +50,7 @@ const Service: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = (
   return (
     <div
       className={clsx(
-        "mt-20 mb-10 flex flex-col lg:flex-row lg:flex-wrap p-4",
+        "mt-20 mb-10 flex flex-col lg:flex-row lg:flex-wrap p-4 justify-around",
         {
           [outfit.className]: true,
         }
@@ -59,18 +61,7 @@ const Service: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = (
         description={props.description || `${props.user.name} on Lend My Skill`}
         ogImage={assetURLBuilder(props.bannerImage)}
       />
-      <div>
-        <div className="flex items-center justify-center">
-          <Image
-            src={assetURLBuilder(props.bannerImage)}
-            alt="Banner Image"
-            className="mb-4 w-full max-w-[400px] aspect-square"
-            classNames={{
-              image:
-                "min-w-[140px] min-h-[140px] max-w-full min-h-full rounded-md object-contain w-full max-w-[400px]",
-            }}
-          />
-        </div>
+      <div className="flex-[0.5]">
         <h1 className="text-2xl font-bold text-center">{props.title}</h1>
         {props.user.profileCompleted ? null : (
           <div className="mt-2  text-center">
@@ -81,50 +72,62 @@ const Service: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = (
             </span>
           </div>
         )}
-        <div className="flex flex-row flex-wrap items-center mt-2 justify-center">
-          <Avatar
-            src={
-              props.user.avatarUrl
-                ? assetURLBuilder(props.user.avatarUrl)
-                : profileImageRouteGenerator(props.user.username)
-            }
-            size="md"
-            radius="xl"
-          />
-          <div className="flex flex-col ml-2">
-            <h2 className="text-base font-semibold">
-              {props.user.name}
-              {props.user.verified ? (
-                <Tooltip label="Verified User" withArrow>
-                  <Badge
-                    color="green"
-                    variant="light"
-                    className="rounded-full ml-2"
-                  >
-                    <div className="flex flex-row flex-nowrap items-center justify-center">
-                      <IconCheck />
-                    </div>
-                  </Badge>
-                </Tooltip>
-              ) : null}
-            </h2>
-            <Text
-              color={"dimmed"}
-              className={clsx("leading-3", {
-                [outfit.className]: true,
-              })}
-            >
-              <a
-                href={`/profile/${props.user.username}`}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-500 text-sm hover:underline"
+        <div className="flex flex-col items-center mt-2 justify-center">
+          <div className="flex flex-row flex-wrap">
+            <Avatar
+              src={
+                props.user.avatarUrl
+                  ? assetURLBuilder(props.user.avatarUrl)
+                  : profileImageRouteGenerator(props.user.username)
+              }
+              size="md"
+              radius="xl"
+            />
+            <div className="flex flex-col ml-2">
+              <h2 className="text-base font-semibold">
+                {props.user.name}
+                {props.user.verified ? (
+                  <Tooltip label="Verified User" withArrow>
+                    <Badge
+                      color="green"
+                      variant="light"
+                      className="rounded-full ml-2"
+                    >
+                      <div className="flex flex-row flex-nowrap items-center justify-center">
+                        <IconCheck />
+                      </div>
+                    </Badge>
+                  </Tooltip>
+                ) : null}
+              </h2>
+              <Text
+                color={"dimmed"}
+                className={clsx("leading-3", {
+                  [outfit.className]: true,
+                })}
               >
-                @{props.user.username}
-              </a>
-            </Text>
+                <a
+                  href={`/profile/${props.user.username}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-500 text-sm hover:underline"
+                >
+                  @{props.user.username}
+                </a>
+              </Text>
+            </div>
+            <div className="flex flex-row items-center ml-2">
+              <Rating
+                fractions={3}
+                defaultValue={Number(Number(props.totalRating).toFixed(2))}
+              />
+              <Text>
+                {props.totalRating} ({props.totalReviews})
+              </Text>
+            </div>
           </div>
-          <div className="flex flex-col items-center justify-center w-full">
+
+          <div className="flex flex-col items-center justify-center w-fit p-4">
             <Divider orientation="horizontal" className={clsx("w-full my-4")} />
             <Badge key={props.category.id}>
               <Link href={`/category/${props.category.slug}`}>
@@ -132,16 +135,21 @@ const Service: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = (
               </Link>
             </Badge>
             {props.tags?.length > 0 ? (
-              <div className="flex flex-row gap-2 items-center justify-center flex-wrap my-4">
-                {props.tags?.map((t) => (
-                  <Badge variant="light" key={t.id} color="green">
-                    <a href={`/services/tags/${t.slug}`}>#{t.name}</a>
-                  </Badge>
-                ))}
-              </div>
+              <>
+                <div className="flex flex-row gap-2 items-center justify-center flex-wrap my-4">
+                  {props.tags?.map((t) => (
+                    <Badge variant="light" key={t.id} color="green">
+                      <a href={`/services/tags/${t.slug}`}>#{t.name}</a>
+                    </Badge>
+                  ))}
+                </div>
+                <Divider
+                  orientation="horizontal"
+                  className={clsx("w-full my-4")}
+                />
+              </>
             ) : null}
           </div>
-          <Divider orientation="horizontal" className={clsx("w-full my-4")} />
           {props.images.length > 0 ? (
             <div className="container max-w-[400px] h-fit">
               <Carousel
@@ -199,128 +207,126 @@ const Service: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = (
             </div>
           ) : null}
           <Divider orientation="horizontal" className={clsx("w-full my-4")} />
-
-          <TypographyStylesProvider>
-            <div
-              dangerouslySetInnerHTML={sanitize(props.description, undefined)}
+          <div className="w-full">
+            <Text
+              className={clsx("font-bold text-2xl text-center mb-4", {
+                [outfit.className]: true,
+              })}
+            >
+              About the service
+            </Text>
+            <Editor
+              onSubmit={() => {}}
+              setActive={() => {}}
+              read
+              content={props.description}
+              editorStyles={{
+                border: "none",
+              }}
             />
-          </TypographyStylesProvider>
+          </div>
         </div>
         <Divider
           orientation="horizontal"
           className={clsx("w-full my-4 lg:hidden")}
         />
       </div>
-      <div>
-        <Text
-          className={clsx("font-bold text-2xl text-center mb-4", {
-            [outfit.className]: true,
-          })}
-          id="packages-offered"
-        >
-          Packages Offered
-        </Text>
+      <div className="flex-[0.25] ml-6">
+        <Tabs
+          unstyled
+          styles={(theme) => ({
+            tab: {
+              ...theme.fn.focusStyles(),
+              backgroundColor:
+                theme.colorScheme === "dark"
+                  ? theme.colors.dark[6]
+                  : theme.white,
+              color:
+                theme.colorScheme === "dark"
+                  ? theme.colors.dark[0]
+                  : theme.colors.gray[9],
+              border: `1px solid ${
+                theme.colorScheme === "dark"
+                  ? theme.colors.dark[6]
+                  : theme.colors.gray[4]
+              }`,
+              padding: `${theme.spacing.xs}px ${theme.spacing.md}px`,
+              cursor: "pointer",
+              fontSize: theme.fontSizes.sm,
+              display: "flex",
+              alignItems: "center",
 
-        <Table striped highlightOnHover withBorder withColumnBorders>
-          <thead>
-            <tr>
-              <th>Package</th>
-              {props.package.map((p) => (
-                <th key={p.id}>{upperFirst(p.name)}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Description</td>
-              {props.package.map((p) => (
-                <td key={p.id}>{p.description || "Not Provided"}</td>
-              ))}
-            </tr>
-            <tr>
-              <td>Delivery</td>
-              {props.package.map((p) => (
-                <td key={p.id}>
-                  {p.deliveryDays} {p.deliveryDays > 1 ? "Days" : "Day"}
-                </td>
-              ))}
-            </tr>
-            <tr>
-              <td>Features</td>
-              {props.package.map((p) => (
-                <td key={p.id}>
-                  {p.features.map((f) => (
-                    <div key={f.id}>
-                      {f.includedIn.includes(p.name) ? (
-                        <div className="flex flex-row items-center gap-2">
-                          <IconCheck size={13} color="green" />
-                          <span>{f.name}</span>
-                        </div>
-                      ) : (
-                        <div className="flex flex-row items-center gap-2">
-                          <IconX size={13} color="red" />
-                          <span>{f.name}</span>
-                        </div>
-                      )}
-                    </div>
-                  ))}
-                </td>
-              ))}
-            </tr>
-            <tr>
-              <td>Price</td>
-              {props.package.map((p) => (
-                <td key={p.id}>
-                  <span className="font-semibold text-[18px]">${p.price}</span>
-                </td>
-              ))}
-            </tr>
-            {props.user.username != username ? (
-              <tr>
-                <td>Actions</td>
-                {props.package.map((p) => (
-                  <td key={p.id}>
-                    <Button
-                      variant="default"
-                      size="sm"
-                      fullWidth
-                      disabled={username === props.user.username}
-                      component={Link}
-                      href={{
-                        pathname: "/service/[slug]/purchase",
-                        query: {
-                          slug: props.slug,
-                          packageId: p.id,
-                        },
-                      }}
-                      color="black"
-                      className={clsx(
-                        "rounded-md text-white transition-all duration-[110ms] hover:scale-105",
-                        {
-                          [outfit.className]: true,
-                          "bg-gray-900 hover:bg-black ":
-                            colorScheme === "light",
-                          "bg-purple-900 hover:bg-purple-800":
-                            colorScheme === "dark",
-                          "cursor-not-allowed":
-                            username === props.user.username,
-                        }
-                      )}
-                    >
-                      Select
-                    </Button>
-                  </td>
+              "&:disabled": {
+                opacity: 0.5,
+                cursor: "not-allowed",
+              },
+              "&:first-of-type": {
+                borderLeft: 0,
+              },
+              "&:last-of-type": {
+                borderRight: 0,
+              },
+              "&[data-active]": {
+                backgroundColor: theme.colors.blue[7],
+                borderColor: theme.colors.blue[7],
+                color: theme.white,
+              },
+            },
+
+            tabIcon: {
+              marginRight: theme.spacing.xs,
+              display: "flex",
+              alignItems: "center",
+            },
+
+            tabsList: {
+              display: "flex",
+            },
+          })}
+          className="lg:sticky lg:top-20 border-t-0 rounded-md border-[1px] border-[#25262b]"
+        >
+          <Tabs.List>
+            {props.package.map((p) => (
+              <Tabs.Tab key={p.id} value={p.id} className="w-full p-4">
+                {upperFirst(p.name)}
+              </Tabs.Tab>
+            ))}
+          </Tabs.List>
+          {props.package.map((p) => (
+            <Tabs.Panel key={p.id} value={p.id} p="xs">
+              <div className="flex flex-col justify-center">
+                <div className="flex flex-row flex-nowrap justify-between mb-8">
+                  <h1 className="text-xl font-bold">{upperFirst(p.name)}</h1>
+                  <h1 className="text-xl font-bold">${p.price}</h1>
+                </div>
+                <p className="mb-8">{p.description}</p>
+                <div className="flex flex-row justify-between mb-8">
+                  <div className="flex flex-row gap-2 items-center">
+                    <IconClockCheck size={20} />
+                    <Text>
+                      {p.deliveryDays} {p.deliveryDays === 1 ? "day" : "days"}
+                    </Text>
+                  </div>
+                </div>
+                {p.features.map((f) => (
+                  <div key={f.id}>
+                    {f.includedIn.includes(p.name) ? (
+                      <div className="flex flex-row items-center gap-2">
+                        <IconCheck size={13} color="green" />
+                        <span>{f.name}</span>
+                      </div>
+                    ) : (
+                      <div className="flex flex-row items-center gap-2">
+                        <IconX size={13} color="red" />
+                        <span>{f.name}</span>
+                      </div>
+                    )}
+                  </div>
                 ))}
-              </tr>
-            ) : null}
-          </tbody>
-        </Table>
-        {/* {userType === "Client" ? (
-        <Script
-          src={"https://checkout.razorpay.com/v1/checkout.js"}
-          strategy="lazyOnload"
-        />
-      ) : null} */}
+              </div>
+            </Tabs.Panel>
+          ))}
+        </Tabs>
       </div>
     </div>
   );
