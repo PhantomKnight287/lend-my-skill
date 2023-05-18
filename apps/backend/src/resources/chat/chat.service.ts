8014fce60323a009ca09272f23f6cbd139dc4cc2
Gurpal Singh <gillgurpal699@gmail.com>
2023-05-18 23:32:29 +0530

    [backend] create route to get chat info

--- apps/backend/src/resources/chat/chat.service.ts
+++ apps/backend/src/resources/chat/chat.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/services/prisma/prisma.service';
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+  constructor(protected p: PrismaService) {}
+
+  async getChatInfo(orderId: string) {
+    const order = await this.p.order.findUnique({
+      where: {
+        id: orderId,
+      },
+      select: {
+        chat: {
+          select: {
+            createdAt: true,
+            id: true,
+          },
+        },
+        client: {
+          select: {
+            name: true,
+            username: true,
+            avatarUrl: true,
+            id: true,
+            verified: true,
+          },
+        },
+        freelancer: {
+          select: {
+            name: true,
+            username: true,
+            avatarUrl: true,
+            id: true,
+            verified: true,
+          },
+        },
+        orderState: true,
+        id: true,
+        createdAt: true,
+      },
+    });
+    if (!order) throw new HttpException('No order found', 404);
+    return order;
+  }
+}
