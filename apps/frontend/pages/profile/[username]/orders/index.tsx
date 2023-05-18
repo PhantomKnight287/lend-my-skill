03e5da804cdbfe3b42e5a1d46c26111745f79b2d
Gurpal Singh <gillgurpal699@gmail.com>
2023-05-18 18:41:42 +0530

    [frontend] update orders apge

--- apps/frontend/pages/profile/[username]/orders/index.tsx
+++ apps/frontend/pages/profile/[username]/orders/index.tsx
@@ -11,6 +11,7 @@ import {
   Card,
   Loader,
   SimpleGrid,
+  Table,
   Tooltip,
 } from "@mantine/core";
 import { IconCheck } from "@tabler/icons-react";
@@ -20,7 +21,7 @@ import { assetURLBuilder, URLBuilder } from "@utils/url";
 import clsx from "clsx";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { Orders } from "~/types/response/order";
 
 const Orders = () => {
@@ -35,7 +36,7 @@ const Orders = () => {
   } = useInfiniteQuery<Orders>({
     queryKey: ["orders"],
     queryFn: async ({ pageParam = 10 }) => {
-      const data = await fetch(URLBuilder(`/order/list?take=${pageParam}`), {
+      const data = await fetch(URLBuilder(`/orders?take=${pageParam}`), {
         headers: {
           authorization: `Bearer ${readCookie("token")}`,
         },
@@ -46,7 +47,7 @@ const Orders = () => {
     getNextPageParam: (lastPage) => lastPage.next,
     enabled: enabled,
   });
-  const { id, userType, username } = useUser();
+  const { id, username } = useUser();
   const { isReady, replace, asPath } = useRouter();
   useEffect(() => {
     if (!isReady) return;
@@ -92,142 +93,59 @@ const Orders = () => {
                 </p>
               </div>
             ) : null}
-            {orders?.pages.map((page, index) => (
-              <div key={index}>
-                <SimpleGrid
-                  cols={3}
-                  spacing="lg"
-                  breakpoints={[
-                    { maxWidth: "md", cols: 3, spacing: "md" },
-                    { maxWidth: "sm", cols: 2, spacing: "sm" },
-                    { maxWidth: "xs", cols: 1, spacing: "sm" },
-                  ]}
-                >
-                  {page.orders?.map((order) => (
-                    <div key={order.id}>
-                      <Card
-                        withBorder
-                        radius="md"
-                        shadow="xl"
-                        component={Link}
-                        href={`/profile/${username}/order/${order.id}`}
-                      >
-                        <Card.Section p="md">
-                          <p>Id: {order.id}</p>
-                          <p>
-                            Order Completion Status:{" "}
-                            {order.status === "CANCELLED"
-                              ? "Cancelled"
-                              : order.status === "COMPLETED"
-                              ? "Completed"
-                              : "Pending"}
-                          </p>
-                        </Card.Section>
-                        <Card.Section p="md">
-                          <p
-                            className={clsx("text-left font-bold text-xl", {
-                              [outfit.className]: true,
-                            })}
+            <Table striped withBorder withColumnBorders className="">
+              <thead>
+                <tr>
+                  <th>Order Id</th>
+                  <th>State</th>
+                  <th>Freelancer</th>
+                  <th>Client</th>
+                  <th>Coupon Code</th>
+                  <th>Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                {orders.pages.map((p, i) => (
+                  <Fragment key={i}>
+                    {p.orders.map((order) => (
+                      <tr key={order.id}>
+                        <td>
+                          <Link
+                            className="text-blue-500"
+                            href={`/profile/${username}/order/${order.id}`}
                           >
-                            {userType === "client"
-                              ? "You Hired"
-                              : "You Got Hired By"}
-                          </p>
-                          <div className="flex">
-                            {(
-                              userType === "client"
-                                ? order.freelancer
-                                : order.client
-                            ) ? (
-                              <Link
-                                href={`/profile/${
-                                  userType === "client"
-                                    ? order.freelancer.username
-                                    : order.client.username
-                                }`}
-                              >
-                                {
-                                  <div className="flex mt-2 flex-row flex-nowrap items-center">
-                                    <Avatar
-                                      src={
-                                        userType === "client"
-                                          ? order.freelancer.avatarUrl
-                                            ? assetURLBuilder(
-                                                order.freelancer.avatarUrl
-                                              )
-                                            : order.client.avatarUrl
-                                            ? assetURLBuilder(
-                                                order.client.avatarUrl
-                                              )
-                                            : profileImageRouteGenerator(
-                                                order.client.username
-                                              )
-                                          : order.client.avatarUrl
-                                          ? assetURLBuilder(
-                                              order.client.avatarUrl
-                                            )
-                                          : order.freelancer.avatarUrl
-                                          ? assetURLBuilder(
-                                              order.freelancer.avatarUrl
-                                            )
-                                          : profileImageRouteGenerator(
-                                              order.freelancer.username
-                                            )
-                                      }
-                                      radius="xl"
-                                    />
-                                    <div className="flex flex-col ml-4">
-                                      <h2 className="font-bold">
-                                        {userType === "client"
-                                          ? order.freelancer.name
-                                          : order.client.name}
-                                        {(
-                                          userType === "client"
-                                            ? order.freelancer.verified
-                                            : order.client.verified
-                                        ) ? (
-                                          <Tooltip
-                                            label="Verified User"
-                                            withArrow
-                                          >
-                                            <Badge
-                                              color="green"
-                                              variant="light"
-                                              className="rounded-full ml-2"
-                                            >
-                                              <div className="flex flex-row flex-nowrap items-center justify-center">
-                                                <IconCheck
-                                                  color="green"
-                                                  size={15}
-                                                />
-                                              </div>
-                                            </Badge>
-                                          </Tooltip>
-                                        ) : null}
-                                      </h2>
-                                      <p
-                                        className={clsx(
-                                          "text-gray-500 text-sm"
-                                        )}
-                                      >
-                                        @
-                                        {userType === "client"
-                                          ? order.freelancer.username
-                                          : order.client.username}
-                                      </p>
-                                    </div>
-                                  </div>
-                                }
-                              </Link>
-                            ) : null}
-                          </div>
-                        </Card.Section>
-                      </Card>
-                    </div>
-                  ))}
-                </SimpleGrid>
-              </div>
-            ))}
+                            {order.id}
+                          </Link>
+                        </td>
+                        <td>
+                          <Badge>{order.orderState}</Badge>
+                        </td>
+                        <td>
+                          <Link
+                            className="text-blue-500"
+                            href={`/profile/${order.freelancer.username}`}
+                          >
+                            {order.freelancer.name}
+                          </Link>
+                        </td>
+                        <td>
+                          <Link
+                            className="text-blue-500"
+                            href={`/profile/${order.client.username}`}
+                          >
+                            {order.client.name}
+                          </Link>
+                        </td>
+                        <td>{order.couponCode?.code ?? "N/A"}</td>
+                        <td>
+                          <Badge>&#8377;{order.transaction.amount / 100}</Badge>
+                        </td>
+                      </tr>
+                    ))}
+                  </Fragment>
+                ))}
+              </tbody>
+            </Table>
           </>
         )}
       </Container>
