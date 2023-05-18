e40e8e3cc03fd121e891ab9df0818444758c178e
Gurpal Singh <gillgurpal699@gmail.com>
2023-05-18 23:33:29 +0530

    [backend] create routes to get messages and their count

--- apps/backend/src/resources/messages/messages.controller.ts
+++ apps/backend/src/resources/messages/messages.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
   constructor(private readonly messagesService: MessagesService) {}
+
+  @Get(':id/count')
+  async getMessagesCount(@Param('id') orderId: string) {
+    return await this.messagesService.getMessagesCount(orderId);
+  }
+  @Get(':orderId')
+  async getMessages(
+    @Param('orderId') orderId: string,
+    @Query('skip') skip?: string,
+    @Query('take') take?: string,
+  ) {
+    return await this.messagesService.getMessages(orderId, skip, take);
+  }
 }
--- apps/backend/src/resources/messages/messages.module.ts
+++ apps/backend/src/resources/messages/messages.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { MessagesController } from './messages.controller';
+import { PrismaService } from 'src/services/prisma/prisma.service';
 
 @Module({
   controllers: [MessagesController],
-  providers: [MessagesService]
+  providers: [MessagesService, PrismaService],
 })
 export class MessagesModule {}
